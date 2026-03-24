1a341045b650b17018b10226a61ef0b91080d736
Nestor Ngabonziza <ngabonest@gmail.com>
2026-03-24 15:47:05 +0200

    feat: adding asynchronous in fastapi endpoints

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from router import router
 from database import Base, engine
+from contextlib import asynccontextmanager
 
 
-Base.metadata.create_all(bind=engine)
-app = FastAPI()
-app.include_router(router)
+@asynccontextmanager
+async def lifespan(_app:FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router, prefix="/api/notes", tags=["Notes"])
 
